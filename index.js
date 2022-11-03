5547182d157629efbacdd5f237e29822064b1539
Harsh Pandey <96276128+HarshP6421@users.noreply.github.com>
2022-11-03 23:20:27 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var audio1 = new Audio("images/heartbeat.mp3");
 var audio2= new Audio("images/main.mp3");
 
-$(".image").click(function () {
+function object(statement, time) {
+    setTimeout(function (statement, time) {
+        statement;
+    }, time)
+}
+
+$(".image").click(function () {   
     $("h1").html("<p>This is for you</p><p>❤️👇❤️</p>");
     $(".image").css("width","100%");
     $(".image").hide();
     $(".image").attr("src","images/heart.gif");
     $(".image").show();
     audio1.play();
+    $("h1").css("font-size","400%")
         setTimeout(function(){
             $("h1").html("3");
         },4000)
@@ -21,6 +28,7 @@ $(".image").click(function () {
         },6000)
 
         setTimeout(function(){
+            $("h1").css("font-size","200%")
         $("h1").html("<p>Love you Buddy❤️</p>");
         $(".image").hide();
         $(".image").attr("src","images/image_main.jpg");
